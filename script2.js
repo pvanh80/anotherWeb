7ea6c8c7892d7611fc3692c8b78bff3666eed03e
Anh Phan Viet <pvanh80@gmail.com>
2018-02-11 23:24:53 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,10 @@
 var map;
-//var icon = "http://path/to/icon.png";
 var json_res = "https://pvanh80.github.io/anotherWeb/restaurants.json";
-var json_ord = "http://localhost/Wolf-summer-trainee-challenge/orders.json";
 var infowindow = new google.maps.InfoWindow();
 function initialize() {
 
     var mapProp = {
-        center: new google.maps.LatLng(60.170708, 24.938224), //LLANDRINDOD WELLS
+        center: new google.maps.LatLng(60.170708, 24.938224), 
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
@@ -43,22 +41,4 @@ function bindInfoWindow(marker, map, infowindow, strDescription) {
         infowindow.open(map, marker);
     });
 }
-// need to use AJAX to load data from JSON datasets
-function display_order(id){
-		var json_ord = "http://localhost/Wolf-summer-trainee-challenge/orders.json";
-
-	    $.getJSON(json_ord, function(order) {
-	        let count = 0;
-		    $.each(order, function (key, data) {
-		    		if(data.restaurant_id === id){
-		    		 count += 1
-		    		}
-		    		//count = count + 1
-		    });
-		   document.getElementById("ordered").innerHTML = count;
-
-	    });
-	 }
-
-
 google.maps.event.addDomListener(window, 'load', initialize);
